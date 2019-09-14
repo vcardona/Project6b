400c5da7d5f9e9f0f50a2459e464a7a558dec80a
Victor Cardona <victorhugocardonagomez@gmail.com>
2019-09-13 19:10:17 -0500

    Initial Commit

---
+++ Project6b/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project6b
+//
+//  Created by Victor Hugo on 13/09/19.
+//  Copyright © 2019 Vintage Robot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
